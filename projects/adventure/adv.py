d6802c42ba21818fb783377160c089e40522c077
Min-Huang <minh.huang96@gmail.com>
2020-02-21 07:57:45 -0800

    small tests are passed

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -12,9 +12,10 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-map_file = "maps/test_loop.txt"
+# map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+# map_file = "maps/Michael_test.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -41,7 +42,7 @@ repeat until the explored room num reach the num of the room
 # dfs
 # Create an empty stack
 stack=[]
-# Add A PATH TO the starting vertex_id to the stack
+# Add the starting room to the stack
 stack.append(world.starting_room)
 print("herr", stack)
 # Create an empty set to store visited nodes
@@ -52,32 +53,74 @@ while len(stack) > 0:
     room = stack.pop()
     # Check if it's been visited
     # If it has not been visited...
-    if room.name not in visited:
+    if room not in visited:
         # Mark it as visited
-        visited.add(room.name)
+        print("stack",room.name)
+        visited.add(room)
         # check if its neighbors have been visited
         # if not, go to one of the directions
-        print(visited)
-        if room.n_to and room.n_to.name not in visited:
-            traversal_path.append("n")
-            stack.append(room.n_to)
-        elif room.s_to and room.s_to.name not in visited:
-            traversal_path.append("s")
-            stack.append(room.s_to)
-        elif room.w_to and room.w_to.name not in visited:
-            traversal_path.append("w")
-            stack.append(room.w_to)
-        elif room.e_to and room.e_to.name not in visited:
-            traversal_path.append("e")
-            stack.append(room.e_to)
-        
-        else:
-            # if all neighbors have been visited, we use bfs to find the first room that has explored neighbor
-            #
-            print("end")
-        
+    if room.n_to and room.n_to not in visited:
+        traversal_path.append("n")
+        stack.append(room.n_to)
+    elif room.s_to and room.s_to not in visited:
+        traversal_path.append("s")
+        stack.append(room.s_to)
+    elif room.w_to and room.w_to not in visited:
+        traversal_path.append("w")
+        stack.append(room.w_to)
+    elif room.e_to and room.e_to not in visited:
+        traversal_path.append("e")
+        stack.append(room.e_to)
+    
+    elif len(visited) == len(room_graph):
+        break
 
-
+    else:
+        # if all neighbors have been visited, use bfs to find the first room that has explored neighbor
+        # dfs
+        # Create an empty queue
+        queue = []
+        # Add the room(which has non-unexplored neighbor) TO the queue as the starting point of the path
+        queue.append(room)
+        paths = [[]]
+        # While the stack is not empty...
+        while len(queue) > 0:
+            # pop, the first room
+            visited_room = queue.pop(0)
+            path = paths.pop(0)
+            # check this visited_room to see if it has unexplored neighbor
+            if (visited_room.n_to and visited_room.n_to not in visited) or (visited_room.s_to and visited_room.s_to not in visited) or (visited_room.w_to and visited_room.w_to not in visited) or (visited_room.e_to and visited_room.e_to not in visited):
+                print("queue", visited_room.name)
+                queue.clear()
+                print(len(queue))
+                traversal_path.extend(path)
+                print(path)
+                stack.append(visited_room)
+            else:
+                
+                # add neighbor to the queue
+                if visited_room.n_to:
+                    new_path = path.copy()
+                    new_path.append("n")
+                    paths.append(new_path)
+                    queue.append(visited_room.n_to)
+                if visited_room.s_to:
+                    new_path = path.copy()
+                    new_path.append("s")
+                    paths.append(new_path)
+                    queue.append(visited_room.s_to)
+                if visited_room.w_to:
+                    new_path = path.copy()
+                    new_path.append("w")
+                    paths.append(new_path)
+                    queue.append(visited_room.w_to)
+                if visited_room.e_to:
+                    new_path = path.copy()
+                    new_path.append("e")
+                    paths.append(new_path)
+                    queue.append(visited_room.e_to)
+            
+  
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -98,12 +141,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
