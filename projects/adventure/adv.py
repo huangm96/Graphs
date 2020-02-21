2b8dcc3a22e1c6bbe10dc2ef60f43d1fa7150fc1
Min-Huang <minh.huang96@gmail.com>
2020-02-20 17:36:39 -0800

    finished dfs part

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,8 +11,8 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+# map_file = "maps/test_cross.txt"
+map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
@@ -29,6 +29,53 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+'''
+using dfs to reach the depthest room which has non-unexplored neighbor
+from this depthest room, use bfs to find the first room that has unexplored neighbor
+use dfs to go to the end
+use bfs to go back 
+repeat until the explored room num reach the num of the room
+
+
+'''
+# dfs
+# Create an empty stack
+stack=[]
+# Add A PATH TO the starting vertex_id to the stack
+stack.append(world.starting_room)
+print("herr", stack)
+# Create an empty set to store visited nodes
+visited = set()
+# While the stack is not empty...
+while len(stack) > 0:
+    # pop, the first room
+    room = stack.pop()
+    # Check if it's been visited
+    # If it has not been visited...
+    if room.name not in visited:
+        # Mark it as visited
+        visited.add(room.name)
+        # check if its neighbors have been visited
+        # if not, go to one of the directions
+        print(visited)
+        if room.n_to and room.n_to.name not in visited:
+            traversal_path.append("n")
+            stack.append(room.n_to)
+        elif room.s_to and room.s_to.name not in visited:
+            traversal_path.append("s")
+            stack.append(room.s_to)
+        elif room.w_to and room.w_to.name not in visited:
+            traversal_path.append("w")
+            stack.append(room.w_to)
+        elif room.e_to and room.e_to.name not in visited:
+            traversal_path.append("e")
+            stack.append(room.e_to)
+        
+        else:
+            # if all neighbors have been visited, we use bfs to find the first room that has explored neighbor
+            #
+            print("end")
+        
 
 
 # TRAVERSAL TEST
